acbff0691093479cbeb2853fb404ecd5bc30f67d
Matt <hallmatthewd12@gmail.com>
2021-02-01 19:34:43 -0500

    Parts 3 & 4 (Adding IDs and Movement)
    Added an ID enumeration to give every object in the game an ID. Also added a KeyInput class to allow movement. Removed the box class and added the wizard class.

--- Resources.md
+++ Resources.md
@@ -6,3 +6,5 @@ https://www.youtube.com/watch?v=e9jRfgjV4FQ&list=PLWms45O3n--5vDnNd6aiu1CSWX3JlC
 Progress:
 Part 1 - Done 2/1/2021 @ 5:50 pm
 Part 2 - Done 2/1/2021 @ 7:09 pm
+Part 3 - Done 2/1/2021 @ 7:16 pm
+Part 4 - Done 2/1/2021 @ 7:33 pm
--- src/Handler.java
+++ src/Handler.java
@@ -5,6 +5,40 @@ public class Handler {
 	
 	LinkedList<GameObject> object = new LinkedList<GameObject>();     // An array of objects
 	
+	private boolean up = false, down = false, right = false, left = false;
+	
+	public boolean isUp() {
+		return up;
+	}
+
+	public void setUp(boolean up) {
+		this.up = up;
+	}
+
+	public boolean isDown() {
+		return down;
+	}
+
+	public void setDown(boolean down) {
+		this.down = down;
+	}
+
+	public boolean isRight() {
+		return right;
+	}
+
+	public void setRight(boolean right) {
+		this.right = right;
+	}
+
+	public boolean isLeft() {
+		return left;
+	}
+
+	public void setLeft(boolean left) {
+		this.left = left;
+	}
+
 	public void tick() {
 		for(int i = 0; i < object.size(); i ++) {     // Run the all the game objects
 			GameObject tempObject = object.get(i);     // Stores each object inside "tempObject"
