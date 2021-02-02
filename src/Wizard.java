1db937bd076d8521bd97c321471781356a9dd657
Matt <hallmatthewd12@gmail.com>
2021-02-01 20:27:47 -0500

    Part 7 (Added Simple Collisions)

--- src/Wizard.java
+++ src/Wizard.java
@@ -15,6 +15,8 @@ public class Wizard extends GameObject {
 		x += velX;
 		y += velY;
 		
+		collisions();
+		
 		
 		//movement
 		if(handler.isUp()) velY = -5;
@@ -30,6 +32,21 @@ public class Wizard extends GameObject {
 		else if(!handler.isRight()) velX = 0;
 		
 		
+	}
+	private void collisions() {
+		for(int i = 0; i < handler.object.size(); i++) {
+			
+			GameObject tempObject = handler.object.get(i);
+			
+			if(tempObject.getId() == ID.Block) {
+				
+				if(getBounds().intersects(tempObject.getBounds())) {
+					x += velX * -1;
+					y += velY * -1;
+				}
+				
+			}
+		}
 	}
 
 	public void render(Graphics g) {
